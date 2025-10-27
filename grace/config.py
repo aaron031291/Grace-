74dbe8d9fca48b8554266318dd320f68e83ec133
aaron031291 <shipton1234@gmail.com>
2025-10-27 20:11:42 +0000

    update

--- grace/config.py
+++ grace/config.py
@@ -6,5 +6,9 @@ OPENAI_KEY = os.getenv("OPENAI_API_KEY")
 WEBSOCKET_HOST = os.getenv("GRACE_WEBSOCKET_HOST", "0.0.0.0")
 WEBSOCKET_PORT = int(os.getenv("GRACE_WEBSOCKET_PORT", 8765))
 
+# WebSocket Configuration
+WEBSOCKET_HOST = os.getenv("GRACE_WEBSOCKET_HOST", "0.0.0.0")
+WEBSOCKET_PORT = int(os.getenv("GRACE_WEBSOCKET_PORT", 8765))
+
 # Kernel-specific settings can be added below
 # e.g., LEARNING_KERNEL_INTERVAL = 5
